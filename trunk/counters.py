1f434de2310ecf9142fe47afee8e116dd18a4332
jwillp <jwillp@d82aa73a-8d9f-11dd-96c2-15d9d6e723a8>
2008-10-04 13:34:52 +0000

    first commit of counters python code.  just made some changes (turned it into a generator!) but also broke the testing, which needs to be turned into a real testing framework anyhow. So, here it is, the first commit. :) -Will

---
+++ trunk/counters.py
@@ -0,0 +1,341 @@
+#!/usr/bin/python
+from __future__ import division
+from random import random
+
+def debug_print(str,  format_tuple):
+    print str % format_tuple
+
+def no_print(*args):
+    pass
+
+def update_debug(state):
+    if (state):
+        dprint = debug_print
+    else:
+        dprint = no_print
+    return dprint
+
+dprint = update_debug(True)
+
+class Counter(object):
+    def __init__(self,
+                 period,
+                 permit_coverage=0.999,
+                 ignore_zeroes=False,
+                 max_delta_t=None,
+                 max_rate=None):
+        #deal with args
+        self.period = period
+        self.permit_coverage = permit_coverage
+        self.ignore_zeroes = ignore_zeroes
+        self.max_dt = max_delta_t
+        self.max_rate = max_rate
+        # Set up intitial state
+        self.data = []
+        self.last_t = None
+        self.last_v = None
+        self.last_bucket_start = None
+        self.bucket = []
+        # init stats
+        self.count_samples = 0
+        self.count_wraps = 0
+        self.count_bad_timestamps = 0
+
+    def _store_last_sample(self,  timestamp,  value):
+        self.last_t = timestamp
+        self.last_v = value
+
+    def _new_bucket(self,  bucket_start):
+        self.bucket =[]
+        self.last_bucket_start = bucket_start
+
+    def new_count(self,  timestamp,  value):
+        # shorthand names for input args, and grab period into local var here
+        t = timestamp
+        v = value
+        if (v==0 and self.ignore_zeroes):
+            # this has to evolve more
+            raise StopIteration
+            ###return len(self.data)
+        period = self.period
+        this_bucket_start = t - (t  % period)
+        dprint ("Timestamp: %d   Value: %d   ... this bucket is [[%d]]",  (t,  v,  this_bucket_start))
+        # handle initialization
+        if self.last_t is None:
+            self.count_samples += 1
+            self._store_last_sample (t, v)
+            self.last_bucket_start = this_bucket_start
+            raise StopIteration
+            #yield
+            ###return len(self.data)
+        # skip bad data
+        delta_t = t - self.last_t
+        delta_v = v - self.last_v
+        if delta_t <= 0:
+            dprint ("  Warning, timestamp %d is invalid coming after previous timestamp %d. Ignoring this sample.",  (t,  self.last_t))
+            self.count_bad_timestamps += 1
+            raise StopIteration
+            #yield
+            ###return len(self.data)
+        # possibly store last  t and v here? instead of everywhere else?
+        if self.max_dt and (delta_t > self.max_dt):
+            self.count_bad_timestamps += 1
+            dprint ("  Warning, timestamp %d has delta_t of %d, which is larger than max permitted.",  (t,  delta_t))
+            # and store this new (t,v) but do not calculate a rate
+            self._store_last_sample (t, v)
+            self._new_bucket (this_bucket_start)
+            raise StopIteration
+            #yield
+            ###return len(self.data)
+        # initialized now, but handle counter wraps and resets (counter appears to go negative)
+        if delta_v < 0:
+            self.count_wraps += 1
+            dprint ("  Warning, counter reset or wrapped from value %d to %d in %d seconds",  (self.last_v,  v,  delta_t))
+            self._store_last_sample(t,  v)
+            self._new_bucket (this_bucket_start)
+            raise StopIteration
+            #yield
+            ###return len(self.data)
+        self.count_samples += 1
+        this_rate = delta_v / delta_t
+        dprint ("  this rate is %.2f",  (this_rate))
+        if self.max_rate and this_rate > self.max_rate:
+            dprint ("ERROR: rate calculated at time %d is %.2f which is larger than max permitted rate of %.2f",  (t,  this_rate,  self.max_rate))
+            self._store_last_sample (t, v)
+            self._new_bucket (this_bucket_start)
+            raise StopIteration
+            #yield
+            ###return len(self.data)
+        # handle case where we crossed into a new bucket
+        if this_bucket_start != self.last_bucket_start:
+            # first, determine how much coverage we have on the last bucket
+            overlap = period - (self.last_t - self.last_bucket_start)
+            if (overlap > 0):
+                dprint ("  overlap on old bucket [%d] is %d seconds",  (self.last_bucket_start,  overlap))
+                bucket_coverage = overlap / period
+                self.bucket.append ( (this_rate,  bucket_coverage) ) # could just be an accumulator
+                dprint ("  Poll at timestamp %d covers %.2f%% of OLD bucket %d with rate: %.2f",  (t,  (100*bucket_coverage),  self.last_bucket_start,  this_rate))
+            # calculate total bucket value
+            sum = 0
+            sum_percent = 0
+            for (b_val,  b_percent) in self.bucket:
+                weighted_val = b_val * b_percent
+                sum += weighted_val
+                sum_percent += b_percent
+                dprint ("  [%d] B_percent: %.2f   B_val: %.2f  (weighted=%.2f)",  (self.last_bucket_start,  b_percent,  b_val,  weighted_val))
+            dprint ("  [[%d]] rates summed to %.2f,  percentages summed to %.3f",  (self.last_bucket_start,  sum,  sum_percent))
+            if (sum_percent >= self.permit_coverage):
+                ###self.data.append ( (self.last_bucket_start,  sum) )
+                yield ( self.last_bucket_start,  sum )
+            else:
+                dprint ("ERROR: Skipped bucket [[%d]] which summed only to %.3f%%!",  (self.last_bucket_start,  sum_percent))
+            # advance forward one bucket...
+            self._new_bucket (self.last_bucket_start + period)
+            while self.last_bucket_start != this_bucket_start:
+                # generate rates for intermediate missing buckets
+                yield ( self.last_bucket_start,  this_rate )
+                ###self.data.append ( (self.last_bucket_start,  this_rate) )
+                dprint ("   skipped bucket %d had overall rate of %.2f (flat-interpolation/counters)",  (self.last_bucket_start,  this_rate))
+                self.last_bucket_start += period
+            overlap = t - this_bucket_start
+            dprint ("  left over overlap = %d, in bucket %d",  (overlap,  this_bucket_start))
+            bucket_coverage = overlap / period
+            ###???yield ( (this_rate,  bucket_coverage))
+            self.bucket.append ( (this_rate,  bucket_coverage)) # could just be an accumulator
+            # ok, handled previous buckets, whether they had data in them or not
+            self._store_last_sample (t,  v)
+            raise StopIteration
+            ###return len(self.data)
+        if this_bucket_start == self.last_bucket_start:
+            bucket_coverage = (t - self.last_t) / period
+            dprint ("  Poll at timestamp %d (delta_t=%d) covers %.2f%% of NEW bucket %d with rate: %.2f",  (t,  delta_t,  (100*bucket_coverage),  this_bucket_start,  this_rate))
+            self.bucket.append ( (this_rate,  bucket_coverage) )
+        self._store_last_sample (t,  v)
+        raise StopIteration
+        ###return len(self.data)
+
+    def get_rates(self):
+        """This method returns the stored data (and clears it) from a counter object. The result
+        is a list of tuples of (timestamp, rate).  Note that this method clears the stored contents."""
+        d = self.data
+        self.data = []
+        return d
+
+class Counter_NoInterpolation(object):
+    def __init__(self,  period, skip_dupes=False,  permit_coverage=None,  ignore_zeroes=False):
+        #deal with args
+        self.period = period
+        self.ignore_zeroes = ignore_zeroes
+        self.skip_dupes=skip_dupes
+        # Set up intitial state
+        self.data = []
+        self.last_t = None
+        self.last_v = None
+        # init stats
+        self.count_samples = 0
+        self.count_wraps = 0
+        self.count_bad_timestamps = 0
+
+    def new_count(self,  timestamp,  value):
+        # shorthand names for input args, and grab period into local var here
+        t = timestamp
+        v = value
+        if (v==0 and self.ignore_zeroes):
+            return len(self.data)
+        period = self.period
+        this_bucket_start = t - (t  % period)
+        dprint ("C2:Timestamp: %d   Value: %d   ... this bucket is [[%d]]",  (t,  v,  this_bucket_start))
+        # handle initialization
+        if self.last_t is None:
+            self.last_t = t
+            self.last_v = v
+            self.count_samples += 1
+            self.last_bucket_start = this_bucket_start
+            return len(self.data)
+        # skip bad data
+        delta_t = t - self.last_t
+        delta_v = v - self.last_v
+        if delta_t <= 0:
+            print "  C2: Warning, timestamp %d is invalid coming after previous timestamp %d. Ignoring this sample.",  (t,  self.last_t)
+            self.count_bad_timestamps += 1
+            return len(self.data)
+        self.last_t = t
+        self.last_v = v
+        if delta_v < 0:
+            print "  C2: Warning, counter reset or wrapped from value %d to %d in %d seconds", (self.last_v,  v,  delta_t)
+            self.last_bucket_start =this_bucket_start
+            self.count_wraps += 1
+            return len(self.data)
+        self.count_samples += 1
+        this_rate = delta_v / delta_t
+        dprint ("  C2: this rate is %.2f",  (this_rate))
+        self.data.append( (this_bucket_start,  this_rate)) # demonstrably wrong!
+        return (len(self.data))
+
+    def get_rates(self):
+        d = self.data
+        self.data = []
+        last_t = None
+        for i in d:
+            (t, v)=i
+            if last_t is not None:
+                if self.skip_dupes and t == last_t:
+                    continue
+                else:
+                    last_t = t
+            else:
+                last_t = t
+            yield (i)
+
+
+# data that is supposed to be interval 60, but varies under-polled and overpolled
+# list: [ (timestamp, value), ... ]
+polled_data = [
+               (0,  10),
+               (1,  10),
+               (30,  60),
+               (41, 60),
+               (60,  130),
+               (130,  280),
+               (190,  460),
+               (240, 460),
+               (241,  460),
+               (250,  710),
+               (305,  840),
+               (470,  1034),
+               (900,  1630)
+               ]
+
+# Set debug output state
+dprint = update_debug(True)
+print
+c=Counter(period=60)
+c_nointerp = Counter_NoInterpolation(period=60,  skip_dupes=False)
+rd=[]
+rd2=[]
+for t, v in polled_data: # this turns into the function call to generate this dataset
+        for result in c.new_count (t,  v):
+            rd.append (result)
+        ret2 = c_nointerp.new_count(t, v)
+        if ret2:
+            for t, v in c_nointerp.get_rates():
+                rd2.append ( [t, v])
+
+
+# check the integrated value
+sum_rate = 0
+sum_rate2 = 0
+for time, rate in rd:
+    sum_rate += (c.period*rate)
+for time, rate in rd2:
+    sum_rate2 += (c_nointerp.period*rate)
+print "Summed rate1: %.2f" % sum_rate
+print rd
+print "\n"
+print "Summed rate2: %.2f" % sum_rate2
+print rd2
+print "\n"
+
+# Now perform the same experiment but using a random generator
+dprint = update_debug(False)
+
+sum_count = 0
+v_sum = 0
+t = 0
+this_period = 60
+c = Counter(period=this_period)
+c2 = Counter_NoInterpolation(period=this_period)
+output = []
+output2 = []
+max_runs = 200000
+for i in xrange(max_runs):
+    # for first and last datapoint, do not increase v_sum (i.e. dont add to it)
+    if (max_runs-1) > i > 0:
+        v_increase = int(200*random())
+        v_sum += v_increase
+    ret = c.new_count(timestamp=t,  value=v_sum)
+    ret2 = c2.new_count(timestamp=t,  value=v_sum)
+    # if we generated a datapoint(s) (ret=True) then pull them out and put them in output[]
+    if (ret):
+        for t1,  v1 in c.get_rates():
+            output.append ( [t1, v1])
+    if (ret2):
+        for t1,  v1 in c2.get_rates():
+            output2.append ( [t1, v1])
+    # make the last datapoint land on a period-interval exactly to force a final sample
+    if i < (max_runs-1):
+        t_increase = 1 + int(this_period+3-3*random())
+    else:
+        t_increase = this_period - (t % this_period)
+    t += t_increase
+extra_sum = 0
+for b_r,  b_c in c.bucket:
+    extra_sum += b_c * b_r
+extra_sum *= c.period
+print "C1: done with %d random counter increases, extra_sum = %.2f" % (max_runs, extra_sum)
+sum_rate = 0
+count_rate =0
+for time, rate in output:
+    sum_rate += (c.period * rate)
+    count_rate += 1
+avg_rate = sum_rate / count_rate
+print "C1: queued up rates (count_rate=%d) add up to %.25f, but avg rate is %.25f" % (count_rate,  sum_rate,  avg_rate)
+sum_rate += extra_sum
+error = abs(sum_rate - v_sum)
+error_rate = error / max_runs * 100
+print "C1: Total converted rate sum is %.3f over %d counter updates and total counter sum is %d, Error=%.25f, error percent is: %.20f%%" % (sum_rate,  c.count_samples,  v_sum,  error,  error_rate)
+
+print
+
+# make function!
+# no bucket for c2, skip extra_sum
+sum_rate2 = 0
+count_rate2 =0
+for time, rate in output2:
+    sum_rate2 += (c2.period * rate)
+    count_rate2 += 1
+avg_rate2 = sum_rate2 / count_rate2
+print "C2: queued up rates (count_rate=%d) add up to %.25f, but avg rate is %.25f" % (count_rate2,  sum_rate2,  avg_rate2)
+error2 = abs(sum_rate2 - v_sum)
+error_rate2 = error2 / max_runs * 100
+print "C2: Total converted rate sum is %.3f over %d counter updates and total counter sum is %d, Error=%.25f, error percent is: %.20f%%" % (sum_rate2,  c2.count_samples,  v_sum,  error2,  error_rate2)
