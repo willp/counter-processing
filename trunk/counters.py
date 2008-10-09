94fc33592b72e43b0ef8d57b9076dc2934e64b27
jwillp <jwillp@d82aa73a-8d9f-11dd-96c2-15d9d6e723a8>
2008-10-09 01:05:13 +0000

    formatting update to adhere to PEP 8,. style guide for python code...
    i'm sure i've missed some more formatting and style adjustments. and will probably make more non-PEP-8 style dillyo's

--- trunk/counters.py
+++ trunk/counters.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 from __future__ import division
 import sys
-from random import random
 
 # pollute local namespace to save time
 from othercounters import *
@@ -14,7 +13,7 @@ def no_print(*args):
     pass
 
 def update_debug(state):
-    if (state):
+    if state:
         dprint = debug_print
     else:
         dprint = no_print
@@ -25,10 +24,10 @@ dprint = update_debug(True)
 class Counter(object):
     def __init__(self,
                  period,
-                 permit_coverage=0.999,
-                 ignore_zeroes=False,
-                 max_delta_t=None,
-                 max_rate=None):
+                 permit_coverage = 0.999,
+                 ignore_zeroes = False,
+                 max_delta_t = None,
+                 max_rate = None):
         #deal with args
         self.period = period
         self.permit_coverage = permit_coverage
@@ -43,8 +42,8 @@ class Counter(object):
         # init stats
         self.count_samples = 0
         self.count_wraps = 0
-        self.skipped_delta_t=0
-        self.skipped_delta_v=0
+        self.skipped_delta_t = 0
+        self.skipped_delta_v = 0
         self.count_bad_timestamps = 0
 
     def _store_last_sample(self,  timestamp,  value):
@@ -59,23 +58,23 @@ class Counter(object):
         # shorthand names for input args, and grab period into local var here
         t = timestamp
         v = value
-        if (v==0 and self.ignore_zeroes):
+        if v==0 and self.ignore_zeroes:
             # this has to evolve more
             raise StopIteration
         period = self.period
         this_bucket_start = t - (t  % period)
-        dprint ("Timestamp: %d   Value: %d   ... this bucket is [[%d]]",  (t,  v,  this_bucket_start))
+        dprint("Timestamp: %d   Value: %d   ... this bucket is [[%d]]",  (t,  v,  this_bucket_start))
         # handle initialization
         if self.last_t is None:
             self.count_samples += 1
-            self._store_last_sample (t, v)
             self.last_bucket_start = this_bucket_start
+            self._store_last_sample(t, v)
             raise StopIteration
         # skip bad data
         delta_t = t - self.last_t
         delta_v = v - self.last_v
         if delta_t <= 0:
-            dprint ("  Warning, timestamp %d is invalid coming after previous timestamp %d. Ignoring this sample.",  (t,  self.last_t))
+            dprint("  Warning, timestamp %d is invalid coming after previous timestamp %d. Ignoring this sample.",  (t,  self.last_t))
             self.count_bad_timestamps += 1
             raise StopIteration
         # possibly store last  t and v here? instead of everywhere else?
@@ -83,35 +82,35 @@ class Counter(object):
             self.count_bad_timestamps += 1
             self.skipped_delta_t += delta_t # track for later verification
             self.skipped_delta_v += delta_v
-            dprint ("  Warning, timestamp %d has delta_t of %d, which is larger than max permitted.",  (t,  delta_t))
+            dprint("  Warning, timestamp %d has delta_t of %d, which is larger than max permitted.",  (t,  delta_t))
             # and store this new (t,v) but do not calculate a rate
-            self._store_last_sample (t, v)
-            self._new_bucket (this_bucket_start)
+            self._store_last_sample(t, v)
+            self._new_bucket(this_bucket_start)
             raise StopIteration
         # handle counter wraps and resets (counter appears to go negative)
         if delta_v < 0:
             self.count_wraps += 1
-            dprint ("  Warning, counter reset or wrapped from value %d to %d in %d seconds",  (self.last_v,  v,  delta_t))
-            self._store_last_sample(t,  v)
-            self._new_bucket (this_bucket_start)
+            dprint("  Warning, counter reset or wrapped from value %d to %d in %d seconds",  (self.last_v,  v,  delta_t))
+            self._store_last_sample(t, v)
+            self._new_bucket(this_bucket_start)
             raise StopIteration
         self.count_samples += 1
         this_rate = delta_v / delta_t
-        dprint ("  this rate is %.2f",  (this_rate))
+        dprint("  this rate is %.2f",  (this_rate))
         if self.max_rate and this_rate > self.max_rate:
-            dprint ("ERROR: rate calculated at time %d is %.2f which is larger than max permitted rate of %.2f",  (t,  this_rate,  self.max_rate))
-            self._store_last_sample (t, v)
-            self._new_bucket (this_bucket_start)
+            dprint("ERROR: rate calculated at time %d is %.2f which is larger than max permitted rate of %.2f",  (t,  this_rate,  self.max_rate))
+            self._store_last_sample(t, v)
+            self._new_bucket(this_bucket_start)
             raise StopIteration
         # handle case where we crossed into a new bucket
         if this_bucket_start != self.last_bucket_start:
             # first, determine how much coverage we have on the last bucket
             overlap = period - (self.last_t - self.last_bucket_start)
-            if (overlap > 0):
-                dprint ("  overlap on old bucket [%d] is %d seconds",  (self.last_bucket_start,  overlap))
+            if overlap > 0:
+                dprint("  overlap on old bucket [%d] is %d seconds",  (self.last_bucket_start,  overlap))
                 bucket_coverage = overlap / period
-                self.bucket.append ( (this_rate,  bucket_coverage) ) # could just be an accumulator
-                dprint ("  Poll at timestamp %d covers %.2f%% of OLD bucket %d with rate: %.2f",  (t,  (100*bucket_coverage),  self.last_bucket_start,  this_rate))
+                self.bucket.append((this_rate,  bucket_coverage)) # could just be an accumulator
+                dprint("  Poll at timestamp %d covers %.2f%% of OLD bucket %d with rate: %.2f",  (t,  (100 * bucket_coverage),  self.last_bucket_start,  this_rate))
             # calculate total bucket value
             sum = 0
             sum_percent = 0
@@ -119,32 +118,32 @@ class Counter(object):
                 weighted_val = b_val * b_percent
                 sum += weighted_val
                 sum_percent += b_percent
-                dprint ("  [%d] B_percent: %.4f   B_val: %.2f  (weighted=%.2f)",  (self.last_bucket_start,  b_percent,  b_val,  weighted_val))
-            dprint ("  [[%d]] percentages summed to %.4f,   rates summed to %.2f",  (self.last_bucket_start, sum_percent, sum))
-            if (sum_percent >= self.permit_coverage):
-                yield ( self.last_bucket_start,  sum )
+                dprint("  [%d] B_percent: %.4f   B_val: %.2f  (weighted=%.2f)",  (self.last_bucket_start,  b_percent,  b_val,  weighted_val))
+            dprint("  [[%d]] percentages summed to %.4f,   rates summed to %.2f",  (self.last_bucket_start, sum_percent, sum))
+            if sum_percent >= self.permit_coverage:
+                yield (self.last_bucket_start,  sum)
             else:
-                dprint ("ERROR: Skipped bucket [[%d]] which summed only to %.3f%%!",  (self.last_bucket_start,  sum_percent))
+                dprint("ERROR: Skipped bucket [[%d]] which summed only to %.3f%%!",  (self.last_bucket_start,  sum_percent))
             # advance forward one bucket...
-            self._new_bucket (self.last_bucket_start + period)
+            self._new_bucket(self.last_bucket_start + period)
             while self.last_bucket_start != this_bucket_start:
                 # generate rates for intermediate missing buckets
-                yield ( self.last_bucket_start,  this_rate )
-                dprint ("   flat-interpolated bucket %d had overall rate of %.2f (flat-interpolation/counters)",  (self.last_bucket_start,  this_rate))
+                yield (self.last_bucket_start,  this_rate)
+                dprint("   flat-interpolated bucket %d had overall rate of %.2f (flat-interpolation/counters)",  (self.last_bucket_start,  this_rate))
                 self.last_bucket_start += period
             overlap = t - this_bucket_start
             if overlap > 0:
-                dprint ("  left over overlap = %d, in bucket %d (t=%d)",  (overlap,  this_bucket_start,  t))
+                dprint("  left over overlap = %d, in bucket %d (t=%d)",  (overlap,  this_bucket_start,  t))
                 bucket_coverage = overlap / period
-                self.bucket.append ( (this_rate,  bucket_coverage)) # could just be an accumulator
+                self.bucket.append((this_rate,  bucket_coverage)) # could just be an accumulator
             # ok, handled previous buckets, whether they had data in them or not
-            self._store_last_sample (t,  v)
+            self._store_last_sample(t, v)
             raise StopIteration
         if this_bucket_start == self.last_bucket_start:
             bucket_coverage = (t - self.last_t) / period
-            dprint ("  Poll at timestamp %d (delta_t=%d) covers %.2f%% of NEW bucket %d with rate: %.2f",  (t,  delta_t,  (100*bucket_coverage),  this_bucket_start,  this_rate))
-            self.bucket.append ( (this_rate,  bucket_coverage) )
-        self._store_last_sample (t,  v)
+            dprint("  Poll at timestamp %d (delta_t=%d) covers %.2f%% of NEW bucket %d with rate: %.2f",  (t,  delta_t,  (100 * bucket_coverage),  this_bucket_start,  this_rate))
+            self.bucket.append((this_rate, bucket_coverage))
+        self._store_last_sample(t, v)
         raise StopIteration
 
 # data that is supposed to be interval 60, but varies under-polled and overpolled
@@ -168,23 +167,23 @@ polled_data = [
 # Set debug output state
 dprint = update_debug(True)
 print
-c=Counter(period=60)
-c_nointerp = Counter_NoInterpolation(period=60,  skip_dupes=False)
-rd=[]
-rd2=[]
+c = Counter(period = 60)
+c_nointerp = Counter_NoInterpolation(period = 60,  skip_dupes = False)
+rd = []
+rd2 = []
 for t, v in polled_data: # this turns into the function call to generate this dataset
-        for result in c.new_count (t,  v):
-            rd.append (result)
-        for result in c_nointerp.new_count (t,  v):
-            rd2.append (result)
+        for result in c.new_count(t, v):
+            rd.append(result)
+        for result in c_nointerp.new_count(t, v):
+            rd2.append(result)
 
 # check the integrated value
 sum_rate = 0
 sum_rate2 = 0
 for time, rate in rd:
-    sum_rate += (c.period*rate)
+    sum_rate += (c.period * rate)
 for time, rate in rd2:
-    sum_rate2 += (c_nointerp.period*rate)
+    sum_rate2 += (c_nointerp.period * rate)
 print "Summed rate1: %.2f" % sum_rate
 print rd
 print
@@ -195,27 +194,26 @@ print
 dprint = update_debug(False)
 # This is boilerplate for the most part!  Turn it into a test framework
 
-def perform_test (data_generator,  test_name):
+def perform_test(data_generator, test_name):
     period = data_generator.period
-    c = Counter(period=period)
+    c = Counter(period = period)
     integrated_sum = 0
     count_samples = 0
     count_rates = 0
     for t, v in data_generator:
-        count_samples+=1
-        for result_t,  result_rate in c.new_count (t,  v):
+        count_samples += 1
+        for result_t, result_rate in c.new_count(t, v):
             this_isum = result_rate * period
             integrated_sum += this_isum
-            count_rates+=1
-        # send counter to any additional processing algorithms here!
-        # JWP
+            count_rates += 1
+        # send counter to any additional processing algorithms here! -JWP
     counter_sum = data_generator.get_rise()
     print "%s] Processing total %d input samples generated %d output rates." % (test_name,  count_samples,  count_rates)
     print "%s] Observed total counter rise: %d, and integrated-sum is: %.25f" % (test_name,  counter_sum,  integrated_sum)
     absolute_error = abs(counter_sum - integrated_sum)
     percent_error = 0
     if counter_sum > 0:
-        percent_error = absolute_error/counter_sum * 100.0
+        percent_error = absolute_error / counter_sum * 100
         print "%s] Absolute Error is: %.25f.  Percent Error is: %.25f%%" % (test_name,  absolute_error,  percent_error)
     else:
         print "%s] Counter did not increase!  Error is zero.  (absolute error=%.25f)" % (test_name,  absolute_error)
@@ -224,40 +222,41 @@ def perform_test (data_generator,  test_name):
 
 # Now perform tests in the various contexts using the following constants
 period = 60
-num=25000
-avg_rate=13
-rs=143
+num = 25000
+avg_rate = 13
+rs = 143
 
 # i'm still looking for the worst-case input, something that will exercise the most IEEE rounding
 # in the generation or in the result processing
-dataset=TestData (num, period, avg_rate,
-                            time_variance="both",  max_time_variance=0.99,
-                            gap_odds=0.05,  gap_avg_width=50,
-                            avg_rate_variance=120002.5,
-                            random_seed=rs)
-perform_test (dataset,  test_name="Data Set EVIL")
+dataset=TestData(num * 100, period, avg_rate,
+                            time_variance = "both",  max_time_variance = 0.99,
+                            gap_odds = 0.05,  gap_avg_width = 50,
+                            avg_rate_variance = 120002.5,
+                            random_seed = rs)
+perform_test(dataset,  test_name = "Data Set EVIL")
 
-dataset=TestData (num, period, avg_rate, random_seed=rs)
-perform_test (dataset,  test_name="Data Set 1")
+dataset = TestData(num, period, avg_rate, random_seed = rs)
+perform_test(dataset,  test_name = "Data Set 1")
 
-dataset=TestData (num, period, avg_rate, fixed_time_offset=int(period/2), random_seed=rs)
-perform_test (dataset,  test_name="Data Set 2")
+dataset = TestData(num, period, avg_rate, fixed_time_offset = int(period / 2), random_seed = rs)
+perform_test(dataset,  test_name = "Data Set 2")
 
 #dprint = update_debug(True)
-dataset=TestData (num, period, avg_rate, time_variance="positive",  max_time_variance=0.20, random_seed=rs)
-perform_test (dataset,  test_name="Data Set 3")
+dataset = TestData(num, period, avg_rate, time_variance = "positive",  max_time_variance = 0.20, random_seed = rs)
+perform_test(dataset,  test_name = "Data Set 3")
 #dprint = update_debug(False)
 
-dataset=TestData (num, period, avg_rate, time_variance="positive",  max_time_variance=2.0, random_seed=rs)
-perform_test (dataset,  test_name="Data Set 4")
+dataset = TestData(num, period, avg_rate, time_variance = "positive",  max_time_variance = 2.0, random_seed = rs)
+perform_test(dataset,  test_name = "Data Set 4")
 
-dataset=TestData (num, period, avg_rate, time_variance="both",  max_time_variance=0.50, random_seed=rs)
-perform_test (dataset,  test_name="Data Set 5")
+dataset = TestData(num, period, avg_rate, time_variance = "both",  max_time_variance = 0.50, random_seed = rs)
+perform_test(dataset,  test_name = "Data Set 5")
 
-dataset=TestData (num, period, avg_rate, time_variance="negative",  max_time_variance=0.20, random_seed=rs)
-perform_test (dataset,  test_name="Data Set 6")
+dataset = TestData(num, period, avg_rate, time_variance = "negative",  max_time_variance = 0.20, random_seed = rs)
+perform_test(dataset,  test_name = "Data Set 6")
 
 print "Data Set 7]  -- not implemented yet\n"
 
-dataset=TestData (num, period, avg_rate, time_variance="negative",  max_time_variance=0.10,  gap_odds=0.05,  gap_avg_width=period*45, random_seed=rs)
-perform_test (dataset,  test_name="Data Set 8")
+dataset = TestData(num, period, avg_rate, time_variance = "negative",  max_time_variance = 0.10,
+                   gap_odds = 0.05,  gap_avg_width = period * 45, random_seed = rs)
+perform_test(dataset,  test_name = "Data Set 8")
--- trunk/testcounters.py
+++ trunk/testcounters.py
@@ -1,46 +1,46 @@
 import random
 
-# An enum
-VARIANCE_BOTH=0
-VARIANCE_NEGATIVE=1
-VARIANCE_POSITIVE=2
-# and helper dictionary
-VARIANCE_ENUM = dict ( positive=VARIANCE_POSITIVE,  negative=VARIANCE_NEGATIVE,  both=VARIANCE_BOTH)
+# enum with constants and helper dictionary
+VARIANCE_BOTH = 0
+VARIANCE_NEGATIVE = 1
+VARIANCE_POSITIVE = 2
+VARIANCE_ENUM = dict(positive = VARIANCE_POSITIVE, negative = VARIANCE_NEGATIVE,
+                      both = VARIANCE_BOTH)
 
 class TestData(object):
     def __init__(self, num,  period, avg_rate,
-                 random_seed=None, debug=False,
-                 max_time_variance=0, avg_rate_variance=0, time_variance="both",
-                 start_time=0, fixed_time_offset=0, gap_odds=0,  gap_avg_width=None, counter_start=0):
-        self.num=num
-        self.period=period
-        self.max_time_variance=max_time_variance
-        self.time_variance=VARIANCE_ENUM[time_variance]
-        self.avg_rate=avg_rate
-        self.avg_rate_variance=avg_rate_variance
-        self.gap_odds=gap_odds
+                 random_seed = None, debug = False,
+                 max_time_variance = 0, avg_rate_variance = 0, time_variance = "both",
+                 start_time = 0, fixed_time_offset = 0, gap_odds = 0,  gap_avg_width = None, counter_start = 0):
+        self.num = num
+        self.period = period
+        self.max_time_variance = max_time_variance
+        self.time_variance = VARIANCE_ENUM[time_variance]
+        self.avg_rate = avg_rate
+        self.avg_rate_variance = avg_rate_variance
+        self.gap_odds = gap_odds
+        self.counter_start = counter_start
         self.fixed_time_offset = fixed_time_offset
         self.debug = debug
+        self.gap_avg_width = gap_avg_width
         if gap_odds > 0 and gap_avg_width is None:
             raise ValueError,  "Error, \"gap_odds\" is set to %.3f in constructor, but \"gap_avg_width\" is not specified!"
-        self.gap_avg_width=gap_avg_width
         # additional internal state
-        self.count=0
+        self.count = 0
         if random_seed is not None:
             random.seed(random_seed)
-        # initialize time
+        # initialize time and counter
         self.time = start_time
-        # initialize counter
-        self.counter=counter_start + fixed_time_offset
+        self.counter = counter_start + fixed_time_offset
 
     def next(self):
-        if (self.count > self.num):
+        if self.count > self.num:
             raise StopIteration
         self.count += 1
         # Return initial counter value on first iteration
         if self.count == 1:
             return (self.time,  self.counter)
-        if (self.count > self.num):
+        if self.count > self.num:
             # make last sample land perfectly on a multiple of the period
             last_time = self.time
             time_increment = self.period - (last_time % self.period)
@@ -51,22 +51,20 @@ class TestData(object):
                 #we hit a gap!
                 gap_width = int(self.gap_avg_width * random.random())
                 time_increment += gap_width
-                if self.debug: print "GAP OCCURRED: WIDTH: %d seconds  (from %d to %d)" % (gap_width,  self.time,  self.time+time_increment)
+                if self.debug: print "GAP OCCURRED: WIDTH: %d seconds  (from %d to %d)" % (gap_width,  self.time,  self.time + time_increment)
             # Now handle polling variance
             if self.max_time_variance > 0:
                 random_variance = random.random()
                 if self.time_variance == VARIANCE_BOTH:
-                    if (random_variance < 0.5):
+                    if random_variance < 0.5:
                         random_variance = -random_variance
                 if self.time_variance == VARIANCE_NEGATIVE:
                     random_variance = -random_variance
                 time_variance = int(self.period * random_variance * self.max_time_variance)
                 # clip negative time variance to ensure time variance never moves us backwards in time
-                if (time_variance <= -self.period):
+                if time_variance <= -self.period:
                     time_variance = self.period - 1
-                #print "TIME VARIANCE: %d" % time_variance
                 time_increment += time_variance # + self.period
-        ###print ">>> Increasing self.time (%d) to (%d) by increment of %d" % (self.time,  self.time+time_increment,  time_increment)
         self.time += time_increment
         # Deal with counter value increasing
         if self.avg_rate_variance > 0:
@@ -87,14 +85,16 @@ class TestData(object):
         return (self.time,  self.counter)
 
     def get_rise(self):
-        return self.counter - self.fixed_time_offset
+        # when supporting counter wrap, subtract accumulated wrapped total
+        return (self.counter - self.fixed_time_offset - self.counter_start)
 
     def __iter__(self):
         return self
 
 if __name__ == "__main__":
     print "\nTesting output:"
-    drift_poll = TestData (num=1000,  period=60,  max_time_variance=0,  avg_rate=100,  gap_odds=0.05, gap_avg_width=60*20 )
+    drift_poll = TestData(num = 1000,  period = 60,  max_time_variance = 0,  avg_rate = 100,
+                          gap_odds = 0.05, gap_avg_width = 60 * 20)
     for t, c in drift_poll:
         print "%20d :  %12d" % (t,  c)
 
